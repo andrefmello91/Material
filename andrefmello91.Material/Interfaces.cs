429baa5747ee11e4cbd1d192d34a468570a27094
andremello91 <andremello91@gmail.com>
2021-07-24 16:08:32 -0400

    Implemented material interfaces

---
+++ andrefmello91.Material/Interfaces.cs
@@ -0,0 +1,68 @@
+﻿using andrefmello91.OnPlaneComponents;
+using UnitsNet;
+
+namespace andrefmello91.Material
+{
+	/// <summary>
+	///		Interface for uniaxial material.
+	/// </summary>
+	public interface IUniaxialMaterial
+	{
+		/// <summary>
+		///     The cross section area.
+		/// </summary>
+		Area Area { get; }
+
+		/// <summary>
+		///     The force at the cross-section.
+		/// </summary>
+		Force Force { get; }
+
+		/// <summary>
+		///     The strain at the cross-section.
+		/// </summary>
+		double Strain { get; }
+
+		/// <summary>
+		///     The stress at the cross-section.
+		/// </summary>
+		Pressure Stress { get; }
+
+		/// <summary>
+		///     Update strain and calculate stress in this material.
+		/// </summary>
+		/// <param name="strain">Current strain.</param>
+		void Calculate(double strain);
+	}
+
+	/// <summary>
+	///		Interface for biaxial material.
+	/// </summary>
+	public interface IBiaxialMaterial
+	{
+		/// <summary>
+		///     The principal strains at this material.
+		/// </summary>
+		PrincipalStrainState PrincipalStrains { get; }
+
+		/// <summary>
+		///     The principal stresses at this material.
+		/// </summary>
+		PrincipalStressState PrincipalStresses { get; }
+
+		/// <summary>
+		///     The strains at this material.
+		/// </summary>
+		StrainState Strains { get; }
+
+		/// <summary>
+		///     The stresses at this material.
+		/// </summary>
+		StressState Stresses { get; }
+
+		/// <summary>
+		///		The stiffness of this material.
+		/// </summary>
+		MaterialMatrix Stiffness { get; }
+	}
+}
